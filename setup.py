7e0438dbd2a89d2048431b97fc290b5e57467c53
Andrew MacDonald <andrew@maccas.net>
2015-06-15 21:58:47 +1000

    Turn into a package.

---
+++ setup.py
@@ -0,0 +1,43 @@
+import os
+from io import open
+
+import versioneer
+
+from setuptools import setup
+
+requirements = [
+    "phildb",
+    "tornado",
+]
+
+setup(
+    name='phildb_server',
+    version=versioneer.get_version(),
+    cmdclass=versioneer.get_cmdclass(),
+    description='PhilDB timeseries database server',
+    author='Andrew MacDonald',
+    author_email='andrew@maccas.net',
+    license='BSD',
+    url='https://github.com/amacd31/phildb_server',
+    install_requires=requirements,
+    packages = ['phildb_server'],
+    test_suite = 'tests',
+    scripts=[
+        'bin/phildb-server',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'Topic :: Software Development :: Build Tools',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'License :: OSI Approved :: BSD License',
+
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.2',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+    ],
+)
